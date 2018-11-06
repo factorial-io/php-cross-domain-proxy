4b7a2873eac5b3ea4e87a229bea88e47cfe8e2c8
Volkan Flörchinger <volkan@factorial.io>
2018-11-06 20:17:37 +0100

    Fix headers to be case insensitive

--- proxy.php
+++ proxy.php
@@ -15,10 +15,10 @@ if( ! isset($curl_timeout))
 
 
 // Get stuff
-$headers = getallheaders();
+$headers = array_change_key_case(getallheaders());
 $method = __('REQUEST_METHOD', $_SERVER);
-$url = __('X-Proxy-URL', $headers);
-$cookie = __('X-Proxy-Cookie', $headers);
+$url = __('x-proxy-url', $headers);
+$cookie = __('x-proxy-cookie', $headers);
 
 // Check that we have a URL
 if( ! $url)
@@ -29,7 +29,7 @@ if( ! parse_url($url, PHP_URL_SCHEME))
 	http_response_code(403) and exit("Not an absolute URL: $url");
 
 // Check referer hostname
-if( ! parse_url(__('Referer', $headers), PHP_URL_HOST) == $_SERVER['HTTP_HOST'])
+if( ! parse_url(__('referer', $headers), PHP_URL_HOST) == $_SERVER['HTTP_HOST'])
 	http_response_code(403) and exit("Referer mismatch");
 
 // Check whitelist, if not empty
@@ -38,10 +38,10 @@ if( ! empty($whitelist) and ! array_reduce($whitelist, 'whitelist', [$url, false
 
 
 // Remove ignored headers and prepare the rest for resending
-$ignore = ['Cookie', 'Host', 'X-Proxy-URL'];
+$ignore = ['cookie', 'host', 'x-proxy-url'];
 $headers = array_diff_key($headers, array_flip($ignore));
 if($cookie)
-	$headers['Cookie'] = $cookie;
+	$headers['cookie'] = $cookie;
 foreach($headers as $key => &$value)
 	$value = "$key: $value";
 
